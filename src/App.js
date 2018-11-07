7bca5dcbcb017c1b59fdd9cf5d452d464a262a7f
Tirapat <mind1990@gmail.com>
2018-11-07 14:00:04 -0800

    finished Sprint 1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
-import { Switch, Route } from 'react-router-dom';
-import Home from './components/Home';
-import TodosContainer from './containers/TodosContainer';
+// import { Switch, Route } from 'react-router-dom';
+// import Home from './components/Home';
+// import TodosContainer from './containers/TodosContainer';
+import MyRoutes from './config/routes';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header/>
-        <Switch>
-          <Route exact path='/' component={ Home } />
-          <Route exact path='/todos' component={ TodosContainer } />
-        </Switch>
+        { MyRoutes }
       </div>
     );
   }
