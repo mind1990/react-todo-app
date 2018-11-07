8399fa148fdf8e9d280f987bb5bbf039dd20f0d2
Tirapat <mind1990@gmail.com>
2018-11-07 12:20:11 -0800

    init

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { 
+  Switch,
+  Route
+} from 'react-router-dom';
+import Home from './components/Home';
+import TodosContainer from './containers/TodosContainer';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Switch>
+          <Route exact path='/' component={ Home } />
+          <Route exact path='/todos' component={ TodosContainer } />
+        </Switch>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/containers/TodosContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Todos extends Component {
+	render() {
+		return (
+			<div>
+				<h2>I'm your Todos Container</h2>
+			</div>
+		);
+	}
+}
+
+export default Todos;
