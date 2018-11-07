5d9b5c742ab6e432613acc80b1ce6544f1f0f7e3
Tirapat <mind1990@gmail.com>
2018-11-07 14:45:37 -0800

    adds todo.js

--- src/containers/TodosContainer.js
+++ src/containers/TodosContainer.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
+import TodoModel from '../models/Todo';
 
 class TodosContainer extends Component {
 	render() {
+		TodoModel.all().then( (res) => {
+			console.log(res);
+		})
 		return (
 			<div className='todosContainer'>
 				<h2>I'm your Todos Container</h2>
